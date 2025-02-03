681092a82949cd38bb36f3d64503c44aab8eb1b5
PackTrouble <39809058+PackTrouble@users.noreply.github.com>
2025-02-02 23:08:22 -0500

    init commit

---
+++ save_handler.js
@@ -0,0 +1,657 @@
+const default_data = [
+    {
+        "weight": 333,
+        "accepted": false
+    },
+    {
+        "weight": 332,
+        "accepted": false
+    },
+    {
+        "weight": 331,
+        "accepted": false
+    },
+    {
+        "weight": 330,
+        "accepted": false
+    },
+    {
+        "weight": 329,
+        "accepted": false
+    },
+    {
+        "weight": 328,
+        "accepted": false
+    },
+    {
+        "weight": 327,
+        "accepted": false
+    },
+    {
+        "weight": 326,
+        "accepted": false
+    },
+    {
+        "weight": 325,
+        "accepted": false
+    },
+    {
+        "weight": 324,
+        "accepted": false
+    },
+    {
+        "weight": 323,
+        "accepted": false
+    },
+    {
+        "weight": 322,
+        "accepted": false
+    },
+    {
+        "weight": 321,
+        "accepted": false
+    },
+    {
+        "weight": 320,
+        "accepted": false
+    },
+    {
+        "weight": 319,
+        "accepted": false
+    },
+    {
+        "weight": 318,
+        "accepted": false
+    },
+    {
+        "weight": 317,
+        "accepted": false
+    },
+    {
+        "weight": 316,
+        "accepted": false
+    },
+    {
+        "weight": 315,
+        "accepted": false
+    },
+    {
+        "weight": 314,
+        "accepted": false
+    },
+    {
+        "weight": 313,
+        "accepted": false
+    },
+    {
+        "weight": 312,
+        "accepted": false
+    },
+    {
+        "weight": 311,
+        "accepted": false
+    },
+    {
+        "weight": 310,
+        "accepted": false
+    },
+    {
+        "weight": 309,
+        "accepted": false
+    },
+    {
+        "weight": 308,
+        "accepted": false
+    },
+    {
+        "weight": 307,
+        "accepted": false
+    },
+    {
+        "weight": 306,
+        "accepted": false
+    },
+    {
+        "weight": 305,
+        "accepted": false
+    },
+    {
+        "weight": 304,
+        "accepted": false
+    },
+    {
+        "weight": 303,
+        "accepted": false
+    },
+    {
+        "weight": 302,
+        "accepted": false
+    },
+    {
+        "weight": 301,
+        "accepted": false
+    },
+    {
+        "weight": 300,
+        "accepted": false
+    },
+    {
+        "weight": 299,
+        "accepted": false
+    },
+    {
+        "weight": 298,
+        "accepted": false
+    },
+    {
+        "weight": 297,
+        "accepted": false
+    },
+    {
+        "weight": 296,
+        "accepted": false
+    },
+    {
+        "weight": 295,
+        "accepted": false
+    },
+    {
+        "weight": 294,
+        "accepted": false
+    },
+    {
+        "weight": 293,
+        "accepted": false
+    },
+    {
+        "weight": 292,
+        "accepted": false
+    },
+    {
+        "weight": 291,
+        "accepted": false
+    },
+    {
+        "weight": 290,
+        "accepted": false
+    },
+    {
+        "weight": 289,
+        "accepted": false
+    },
+    {
+        "weight": 288,
+        "accepted": false
+    },
+    {
+        "weight": 287,
+        "accepted": false
+    },
+    {
+        "weight": 286,
+        "accepted": false
+    },
+    {
+        "weight": 285,
+        "accepted": false
+    },
+    {
+        "weight": 284,
+        "accepted": false
+    },
+    {
+        "weight": 283,
+        "accepted": false
+    },
+    {
+        "weight": 282,
+        "accepted": false
+    },
+    {
+        "weight": 281,
+        "accepted": false
+    },
+    {
+        "weight": 280,
+        "accepted": false
+    },
+    {
+        "weight": 279,
+        "accepted": false
+    },
+    {
+        "weight": 278,
+        "accepted": false
+    },
+    {
+        "weight": 277,
+        "accepted": false
+    },
+    {
+        "weight": 276,
+        "accepted": false
+    },
+    {
+        "weight": 275,
+        "accepted": false
+    },
+    {
+        "weight": 274,
+        "accepted": false
+    },
+    {
+        "weight": 273,
+        "accepted": false
+    },
+    {
+        "weight": 272,
+        "accepted": false
+    },
+    {
+        "weight": 271,
+        "accepted": false
+    },
+    {
+        "weight": 270,
+        "accepted": false
+    },
+    {
+        "weight": 269,
+        "accepted": false
+    },
+    {
+        "weight": 268,
+        "accepted": false
+    },
+    {
+        "weight": 267,
+        "accepted": false
+    },
+    {
+        "weight": 266,
+        "accepted": false
+    },
+    {
+        "weight": 265,
+        "accepted": false
+    },
+    {
+        "weight": 264,
+        "accepted": false
+    },
+    {
+        "weight": 263,
+        "accepted": false
+    },
+    {
+        "weight": 262,
+        "accepted": false
+    },
+    {
+        "weight": 261,
+        "accepted": false
+    },
+    {
+        "weight": 260,
+        "accepted": false
+    },
+    {
+        "weight": 259,
+        "accepted": false
+    },
+    {
+        "weight": 258,
+        "accepted": false
+    },
+    {
+        "weight": 257,
+        "accepted": false
+    },
+    {
+        "weight": 256,
+        "accepted": false
+    },
+    {
+        "weight": 255,
+        "accepted": false
+    },
+    {
+        "weight": 254,
+        "accepted": false
+    },
+    {
+        "weight": 253,
+        "accepted": false
+    },
+    {
+        "weight": 252,
+        "accepted": false
+    },
+    {
+        "weight": 251,
+        "accepted": false
+    },
+    {
+        "weight": 250,
+        "accepted": false
+    },
+    {
+        "weight": 249,
+        "accepted": false
+    },
+    {
+        "weight": 248,
+        "accepted": false
+    },
+    {
+        "weight": 247,
+        "accepted": false
+    },
+    {
+        "weight": 246,
+        "accepted": false
+    },
+    {
+        "weight": 245,
+        "accepted": false
+    },
+    {
+        "weight": 244,
+        "accepted": false
+    },
+    {
+        "weight": 243,
+        "accepted": false
+    },
+    {
+        "weight": 242,
+        "accepted": false
+    },
+    {
+        "weight": 241,
+        "accepted": false
+    },
+    {
+        "weight": 240,
+        "accepted": false
+    },
+    {
+        "weight": 239,
+        "accepted": false
+    },
+    {
+        "weight": 238,
+        "accepted": false
+    },
+    {
+        "weight": 237,
+        "accepted": false
+    },
+    {
+        "weight": 236,
+        "accepted": false
+    },
+    {
+        "weight": 235,
+        "accepted": false
+    },
+    {
+        "weight": 234,
+        "accepted": false
+    },
+    {
+        "weight": 233,
+        "accepted": false
+    },
+    {
+        "weight": 232,
+        "accepted": false
+    },
+    {
+        "weight": 231,
+        "accepted": false
+    },
+    {
+        "weight": 230,
+        "accepted": false
+    },
+    {
+        "weight": 229,
+        "accepted": false
+    },
+    {
+        "weight": 228,
+        "accepted": false
+    },
+    {
+        "weight": 227,
+        "accepted": false
+    },
+    {
+        "weight": 226,
+        "accepted": false
+    },
+    {
+        "weight": 225,
+        "accepted": false
+    },
+    {
+        "weight": 224,
+        "accepted": false
+    },
+    {
+        "weight": 223,
+        "accepted": false
+    },
+    {
+        "weight": 222,
+        "accepted": false
+    },
+    {
+        "weight": 221,
+        "accepted": false
+    },
+    {
+        "weight": 220,
+        "accepted": false
+    },
+    {
+        "weight": 219,
+        "accepted": false
+    },
+    {
+        "weight": 218,
+        "accepted": false
+    },
+    {
+        "weight": 217,
+        "accepted": false
+    },
+    {
+        "weight": 216,
+        "accepted": false
+    },
+    {
+        "weight": 215,
+        "accepted": false
+    },
+    {
+        "weight": 214,
+        "accepted": false
+    },
+    {
+        "weight": 213,
+        "accepted": false
+    },
+    {
+        "weight": 212,
+        "accepted": false
+    },
+    {
+        "weight": 211,
+        "accepted": false
+    },
+    {
+        "weight": 210,
+        "accepted": false
+    },
+    {
+        "weight": 209,
+        "accepted": false
+    },
+    {
+        "weight": 208,
+        "accepted": false
+    },
+    {
+        "weight": 207,
+        "accepted": false
+    },
+    {
+        "weight": 206,
+        "accepted": false
+    },
+    {
+        "weight": 205,
+        "accepted": false
+    },
+    {
+        "weight": 204,
+        "accepted": false
+    },
+    {
+        "weight": 203,
+        "accepted": false
+    },
+    {
+        "weight": 202,
+        "accepted": false
+    },
+    {
+        "weight": 201,
+        "accepted": false
+    },
+    {
+        "weight": 200,
+        "accepted": false
+    },
+    {
+        "weight": 199,
+        "accepted": false
+    },
+    {
+        "weight": 198,
+        "accepted": false
+    },
+    {
+        "weight": 197,
+        "accepted": false
+    },
+    {
+        "weight": 196,
+        "accepted": false
+    },
+    {
+        "weight": 195,
+        "accepted": false
+    },
+    {
+        "weight": 194,
+        "accepted": false
+    },
+    {
+        "weight": 193,
+        "accepted": false
+    },
+    {
+        "weight": 192,
+        "accepted": false
+    },
+    {
+        "weight": 191,
+        "accepted": false
+    },
+    {
+        "weight": 190,
+        "accepted": false
+    },
+    {
+        "weight": 189,
+        "accepted": false
+    },
+    {
+        "weight": 188,
+        "accepted": false
+    },
+    {
+        "weight": 187,
+        "accepted": false
+    },
+    {
+        "weight": 186,
+        "accepted": false
+    },
+    {
+        "weight": 185,
+        "accepted": false
+    },
+    {
+        "weight": 184,
+        "accepted": false
+    },
+    {
+        "weight": 183,
+        "accepted": false
+    },
+    {
+        "weight": 182,
+        "accepted": false
+    },
+    {
+        "weight": 181,
+        "accepted": false
+    },
+    {
+        "weight": 180,
+        "accepted": false
+    }
+  ]
+const default_settings = {
+    primary_color_value:"#ff0000",
+    secondary_color_value:"#000000",
+    font_color_value:"#000000",
+}
+async function get_data(){
+    let data = JSON.parse(localStorage.getItem("weight_progress_tracker"))
+
+    if(data == undefined){
+        localStorage.setItem("weight_progress_tracker", JSON.stringify(default_data))
+        return {success:false, message:"Error: No Data - Initalizing default settings"}
+    }
+    return data;
+
+}
+function set_data(data){
+    localStorage.setItem("weight_progress_tracker", JSON.stringify(data))
+}
+
+async function get_config(){
+    let data = JSON.parse(localStorage.getItem("weight_progress_tracker_settings"))
+    if(data == undefined){
+        localStorage.setItem("weight_progress_tracker_settings", JSON.stringify(default_settings))
+        return {success:false, message:"Error: No settings - Initalizing default settings"}
+    }
+    return data;
+}
+function set_config(data){
+    localStorage.setItem("weight_progress_tracker_settings", JSON.stringify(data))
+
+}
+
+
+export default {
+    get_data,
+    set_data,
+    get_config,
+    set_config
+}
